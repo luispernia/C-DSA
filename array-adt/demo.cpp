196b821d8b9b874fc3fb78f852a95f8d51e4ad1b
Luis Pernia <luispcode@gmail.com>
2023-10-11 21:22:38 +0200

    delete, append, insert added

--- array-adt/demo.cpp
+++ array-adt/demo.cpp
@@ -1,42 +1,55 @@
-#include <algorithm>
 #include<iostream>
-// #include<stdio.h>
+#include<stdio.h>
 
-using namespace std;
 
 struct Array {
-
-    int *A;
-    int length;
-    int size;
-
+	int A[20];
+	int size;
+	int length;
 };
 
 void Display(struct Array arr) {
-    int i;
-    for(i = 0; i < arr.length; i++) {
-        printf("%d", arr.A[i]);
-    }
-};
+	int i;
+	printf("Elements are\n");
+	for (i = 0; i < arr.length; i++)
+		printf("%d", arr.A[i]);
+}
+
+void Append(struct Array* arr, int x) {
+	if (arr->length < arr->size)
+		arr->A[arr->length++] = x;
+}
+
+void Insert(struct Array* arr, int index, int x) {
+	int i;
+	if (index >= 0 && index <= arr->length) {
+		for (i = arr->length; i > index; i--)
+			arr->A[i] = arr->A[i - 1];
+		arr->A[index] = x;
+		arr->length++;
+	}		
+}
+
+int Delete(struct Array* arr,int index) {
+	if (index >= 0 && index < arr->length) {
+		int temp = arr->A[index];
+		for (int i = index; i < arr->length - 1; i++)
+			arr->A[i] = arr->A[i + 1];
+		arr->length--;
+		printf("%d Deleted \n", temp);
+		return temp;
+	}
+
+	return 0;
+}
 
 int main() {
+	struct Array arr = { {2,3,4,5,6}, 20, 5};
+	Insert(&arr, 3, 10);
+	Delete(&arr, 3);
+	Delete(&arr, 3);
+	Delete(&arr, 3);
+	Display(arr);
 
-    struct Array arr;
-    int n,i;
-    printf("Enter size of an array \n");
-    scanf("%d", &arr.size);
-    arr.A = (int *) malloc(arr.size * sizeof(int));
-    arr.length = 0;
-
-    printf("Enter number of insertionss");
-
-    scanf("%d", &n);
-    arr.length = n;
-
-    for(i = 0; i < n; i++)
-        scanf("%d", &arr.A[n]);
-
-    Display(arr);
-
-    return 0;
+	return 0;
 }
